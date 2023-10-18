53c84d307689d51af85634cea16cf1267eeb6d6a
Cacho Medina <zanlorenzocacho10@gmail.com>
2023-10-18 19:05:24 -0300

    index

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+
+
+Ejercicio 1: Variables y Operaciones Matemáticas
+
+Crea dos variables, numero1 y numero2, y asígnales valores numéricos. Luego, realiza las siguientes operaciones y muestra los resultados en la consola:
+
+Suma de numero1 y numero2.
+Resta de numero1 y numero2.
+Multiplicación de numero1 y numero2.
+División de numero1 entre numero2.
+
+*/
+
+const numero1 = 1;
+const numero2 = 2;
+
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+
+/*
+
+Ejercicio 2: Concatenación de Cadenas
+
+Crea dos variables, nombre y apellido, y asígnales valores de tu elección. Luego, crea una tercera variable llamada nombreCompleto que contenga la concatenación de nombre y apellido. Muestra nombreCompleto en la consola.
+
+Ejercicio 3: Conversión de Grados Celsius a Fahrenheit
+
+Crea una variable llamada gradosCelsius y asígnale un valor en grados Celsius. Luego, utiliza la fórmula de conversión para calcular los grados Fahrenheit y almacénalos en una variable llamada gradosFahrenheit. Muestra ambos valores en la consola.
+
+‌
+
+
+*/
